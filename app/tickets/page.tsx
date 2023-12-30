6de0d4c65368b46a617c1586e2436296b444701f
Jake Home <jake@claritycoders.com>
2023-12-30 15:41:25 -0600

    Viewing Tickets

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import prisma from "@/prisma/db";
+import DataTable from "./DataTable";
 
-const Tickets = () => {
-  return <div>Tickets</div>;
+const Tickets = async () => {
+  const tickets = await prisma.ticket.findMany();
+
+  return (
+    <div>
+      <DataTable tickets={tickets} />
+    </div>
+  );
 };
 
 export default Tickets;
