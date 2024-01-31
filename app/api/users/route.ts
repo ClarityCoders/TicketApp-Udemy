2d74a39eb3a5aea86972883e0e987c4fc7e78cfe
Jake Home <jake@claritycoders.com>
2024-01-30 21:00:56 -0600

    Post User API

---
+++ app/api/users/route.ts
@@ -0,0 +1,35 @@
+import { userSchema } from "@/ValidationSchemas/users";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/db";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = userSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), { status: 400 });
+  }
+
+  const duplicate = await prisma.user.findUnique({
+    where: {
+      username: body.username,
+    },
+  });
+
+  if (duplicate) {
+    return NextResponse.json(
+      { message: "Duplicate Username" },
+      { status: 409 }
+    );
+  }
+
+  const hashPassword = await bcrypt.hash(body.password, 10);
+  body.password = hashPassword;
+
+  const newUser = await prisma.user.create({
+    data: { ...body },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
+}
